4de337e030800ffe81881723439fe2e38bcb2df5
VeronicaMG <33263432+VeronicaLobos@users.noreply.github.com>
2025-01-20 08:25:38 +0100

    Step 1. access data from json file

--- main.py
+++ main.py
@@ -3,25 +3,25 @@ import load_animals_data
 ANIMALS_DATA = load_animals_data.animals_data_list
 
 
-def get_animal_info(data):
+def get_print_animal_info(data):
     """
-   :param data:
-   :return: A list of tuples
+   From a list of dictionaries, gets certain values,
+   and prints a formated string for each dictionary.
    """
-    animal_info_list = []
     for animal in data:
-        name = f"Name: {animal['name']}\n"
-        diet = f"Diet: {animal['characteristics'].get('diet')}\n"
-        location = f"Location: {animal['locations'][0]}\n"
-        type = f"Type: {animal['characteristics'].get('type')}\n\n"
+        name = animal['name']
+        diet = animal['characteristics'].get('diet')
+        location = animal['locations'][0]
+        type = animal['characteristics'].get('type')
 
-        animal_info = (name, diet, location)
+        animal_info = f"Name: {name}\nDiet: {diet}\nLocation: {location}\n"
         if type is not None:
-            animal_info += (type,) #new to think a bit more about this
+            animal_info += f"Type: {type}\n"
 
-        animal_info_list.append(animal_info)
+        print(animal_info)
 
-    print(animal_info_list)
+def main():
+    get_print_animal_info(ANIMALS_DATA)
 
-
-get_animal_info(ANIMALS_DATA)
+if __name__ == "__main__":
+    main()
