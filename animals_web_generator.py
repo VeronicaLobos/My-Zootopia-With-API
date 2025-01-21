d3c724e9b803971463cdf5c53d01e2df0135a5ff
VeronicaMG <33263432+VeronicaLobos@users.noreply.github.com>
2025-01-21 21:54:19 +0100

    Extra Step. Formatting your code

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,56 +1,56 @@
 import json
 
-def replace_animals_info_html(old_string, html_template, new_html_file_name, txt_file):
+
+def replace_animals_info_html(old_string, html_template, new_html_file_name, new_string):
     """
     Reads a html template and a txt, replaces a string from the template
     (old_string) with the contents from the txt
     Returns/overwrites a new html file with the changes
     """
-    with open(txt_file, "r") as handle:
-        replacement_text = handle.read()
-
     with open(html_template, "r") as handle:
         html_content = handle.read()
 
-    new_html_file = html_content.replace(old_string, replacement_text)
+    new_html_file = html_content.replace(old_string, new_string)
 
     with open(new_html_file_name, 'w') as handle:
         handle.write(new_html_file)
 
 
-def write_animals_data(string):
-    """
-    Writes a html formated string and
-    returns it as a txt file
-    """
-    with open("replacement_text.txt", "w") as file:
-        file.write(string)
-
-
 def get_animal_info_cards(data):
     """
-   From a list of dictionaries, gets certain non-None values
-   Returns a html formated string for each dictionary (each as a card)
-   """
-    animal_cards = ""
+    Gets certain non-None info from each animal dictionary in the data and
+    calls serialize_animal to format the info for each animal as a html card
+    Returns a string
+    """
+    serialized_animal_objects = ""
     for animal in data:
         name = animal['name']
         diet = animal['characteristics'].get('diet')
         location = animal['locations'][0]
         type_maybe = animal['characteristics'].get('type')
+        animal_object = [name, diet, location, type_maybe]
+        serialized_animal_objects += serialize_animal_info(animal_object)
 
-        animal_cards += (f"<li class='cards__item'>\n"
-                   f"\t\t\t\t<div class='card__title'>{name}</div>\n"
-                   f"\t\t\t\t<p class='card__text'>\n"
-                   f"\t\t\t\t\t<strong>Diet:</strong> {diet}<br/>\n"
-                   f"\t\t\t\t\t<strong>Location:</strong> {location}<br/>\n")
-        if type_maybe is not None:
-            animal_cards += f"\t\t\t\t\t<strong>Type:</strong> {type_maybe}<br/>\n"
-        animal_cards += (f"\t\t\t\t<p>\n"
-                         f"\t\t\t</li>\n"
-                         f"\t\t\t")
+    return serialized_animal_objects
 
-    return animal_cards[:-4]
+
+def serialize_animal_info(animal_object):
+    """
+    Serializes and returns an animal card from an animal object
+    """
+    animal_card = ""
+    animal_card += (f"<li class='cards__item'>\n"
+                    f"\t\t\t\t<div class='card__title'>{animal_object[0]}</div>\n"
+                    f"\t\t\t\t<p class='card__text'>\n"
+                    f"\t\t\t\t\t<strong>Diet:</strong> {animal_object[1]}<br/>\n"
+                    f"\t\t\t\t\t<strong>Location:</strong> {animal_object[2]}<br/>\n")
+    if animal_object[3] is not None:
+        animal_card += f"\t\t\t\t\t<strong>Type:</strong> {animal_object[3]}<br/>\n"
+    animal_card += (f"\t\t\t\t<p>\n"
+                    f"\t\t\t</li>\n"
+                    f"\t\t\t")
+
+    return animal_card
 
 
 def load_animal_data(file_path):
@@ -63,12 +63,11 @@ def load_animal_data(file_path):
 
 def main():
     animals_data = load_animal_data('animals_data.json') # as a list of dictionaries
-    animals_info_cards = get_animal_info_cards(animals_data)
-    write_animals_data(animals_info_cards) #added an extra just  because
+    new_string = get_animal_info_cards(animals_data)
     string_to_be_replaced = "__REPLACE_ANIMALS_INFO__" # it would be better as user input but hardcoded it is
     new_html_file_name = "animals.html" # as a variable in main in case I need to change the name again
     replace_animals_info_html(string_to_be_replaced, "animals_template.html",
-                              new_html_file_name,"replacement_text.txt")
+                              new_html_file_name, new_string)
 
 
 if __name__ == "__main__":
