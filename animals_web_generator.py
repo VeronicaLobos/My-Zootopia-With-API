aa0715459002297a2fb9310d4a58a6cadfc33eba
VeronicaMG <33263432+VeronicaLobos@users.noreply.github.com>
2025-02-02 11:25:57 +0100

    Fixed bugs for Milestone 3

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,4 +1,7 @@
 import json
+from logging import raiseExceptions
+from operator import is_not
+
 import load_json
 
 
@@ -68,14 +71,53 @@ def load_animal_data(file_path):
         return json.load(handle)
 
 
+def get_user_input():
+  """
+  Gets user input, checks that the user didn't input an empty string,
+  in which case will prompt the user to return a valid input.
+  Returns  a string.
+  """
+  while True:
+    user_input = input("What animal name would you like to search for? ")
+    if len(user_input) > 0:
+        return user_input
+    else:
+        print("Please, enter a valid input before pressing return.")
+        continue
+
+
 def main():
-    load_json.fetch_json_file()
-    animals_data = load_animal_data("animal.json") # as a list of dictionaries
-    new_string = get_animal_info_cards(animals_data)
-    string_to_be_replaced = "__REPLACE_ANIMALS_INFO__" # it would be better as user input but hardcoded it is
-    new_html_file_name = "animals.html"  # as a variable in main in case I need to change the name again
-    replace_animals_info_html(string_to_be_replaced, "animals_template.html",
-                              new_html_file_name, new_string)
+    """
+    1. Gets user input to search for an animal name at Animal API
+    2. Makes a request to Animal API and saves it into a json file
+    3. Loads the json file to a variable as a list of dictionaries
+    If the list isn't empty:
+    4. Parses the list of dictionaries into a string containing html cards
+    5. Generates the html file from a template, by replacing a
+        hardcoded string with the html cards
+    Otherwise:
+    6. Generates a "not found" html file, by replacing the text
+        with a header indicating the non-existing input
+
+    :return:
+    """
+    string_to_be_replaced = "__REPLACE_ANIMALS_INFO__"
+
+    animal_name = get_user_input()  # Milestone 2
+    load_json.fetch_json_file(animal_name)  # Milestone 1
+    animals_data = load_animal_data("animal.json")
+
+    if len(animals_data) > 2:
+        new_string = get_animal_info_cards(animals_data)
+        replace_animals_info_html(string_to_be_replaced, "animals_template.html",
+                              f"{animal_name}.html", new_string)
+        print("Zootopia with API successfully generated!")
+    else:  # Milestone 3
+        not_found_string = f"<h2>The animal <em>{animal_name}</em> doesn't exist...</h2>"
+        print(f"{animal_name} not found...")
+        replace_animals_info_html(string_to_be_replaced, "animals_template.html",
+                                  "not_found.html", not_found_string)
+        print("But at least the program didn't crash!")
 
 
 if __name__ == "__main__":
--- load_json.py
+++ load_json.py
@@ -10,38 +10,39 @@ API_KEY = os.environ.get("my_api_key")
 URL = "https://api.api-ninjas.com/v1/animals"
 
 
-def get_animal_info():
+def get_animal_info(animal_name):
     """
-    Checks for the status
-    :return: A list of
+
+    :param animal_name: The name of the animal to look for
+    :return: A json response as a string if requests is successful,
+    an error message indicating
     """
     headers = {"X-Api-Key" : API_KEY}
-    payload = {"name": input("What animal would you like to inquire about? ")}
-    # make this input in the main script later
+    payload = {"name": animal_name}
 
-    animal_query = req.get(URL, payload).url
-    response = req.get(animal_query, headers=headers)
-
-    if response.status_code == 200:
-        print(f"{animal_query} is green, proceed")
-        return str(response.text)
-
-    else:
-        print(f"Error: {response.status_code}")
-
-
-def check_input(content):
-    if content == '[]':
-        print("Animal name not found.")
+    try:
+        animal_query = req.get(URL, payload).url
+        response = req.get(animal_query, headers=headers)
+        response.raise_for_status()
+        print(f"Requesting '{animal_name}' to {URL}")
+        return response.text
+    except ValueError as e:
+        print(f"Error: {e}")
+        return
+    except NameError as e:
+        print(f"Error: {e}")
+        return "Check URL and API key and try again."
 
 
 def save_json_file(json_obj):
+    """
+    """
     with open("animal.json", "w", encoding="utf-8") as handle:
         handle.write(json_obj)
 
 
-def fetch_json_file():  # change name duh
-    animal_json = get_animal_info()
-    #print(animal_json)
-    check_input(animal_json)
+def fetch_json_file(animal_name):  # change name duh
+    """
+    """
+    animal_json = get_animal_info(animal_name)
     save_json_file(animal_json)
